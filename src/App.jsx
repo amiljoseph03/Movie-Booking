dd080452a3a8dd6326015ceec2c4ccb3867b9ae8
amiljoseph03 <amiljoseph03@gmail.com>
2025-10-21 23:27:32 +0530

    movie booking website

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/movies' element={<Movies/>} />
+       
+      </Routes>
+    </>
+  )
+}
+
+export default App
