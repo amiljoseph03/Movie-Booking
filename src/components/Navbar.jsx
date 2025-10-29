80836d9e3d0e4bf849ed2ae99e8238fbc530b883
amiljoseph03 <amiljoseph03@gmail.com>
2025-10-29 12:13:43 +0530

    updated button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { MenuIcon, SearchIcon, XIcon } from 'lucide-react';
 
 
 
+
 const Navbar = () => {
   return (
     <div
@@ -14,32 +15,37 @@ const Navbar = () => {
     md:px-16 lg:px-36 py-5"
     >
       {/* <h1>Navbar</h1> */}
-      <Link to='/' className='max-md:flex-1'>
-        <img src={mylogo}alt=""  className='w-36 h-auto'/>
+      <Link to="/" className="max-md:flex-1">
+        <img src={mylogo} alt="" className="w-36 h-auto" />
       </Link>
 
-      <div className='max-md:absolute max-md:top-0
+      <div
+        className="max-md:absolute max-md:top-0
       max md-left-0 max-md:font-medium max-md:text-lg z-50
       flex flex-col md:flex-row items bg-center
       max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen
       min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border
-      border-gray-300/20 overflow-hidden transition-[width] duration-300'>
-        <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer'/>
-        <Link to='/'> Home</Link>
-        <Link to='/movies'> Movies</Link>
-        <Link to='/'> Theaters</Link>
-        <Link to='/'> Releases</Link>
-        <Link to='/fav'> Favorites</Link>
+      border-gray-300/20 overflow-hidden transition-[width] duration-300"
+      >
+        <XIcon className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer" />
+        <Link to="/"> Home</Link>
+        <Link to="/movies"> Movies</Link>
+        <Link to="/"> Theaters</Link>
+        <Link to="/"> Releases</Link>
+        <Link to="/fav"> Favorites</Link>
       </div>
 
-      <div className='flex items-center gap-8'>
-        <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
-        <button className='px-4 py-1 sm:px-7 sm:py-2
-        bg-primary hover:bg-primary-dull transition rounded-full
-        font-medium cursor-pointer'>Login</button>
-        
+      <div className="flex items-center gap-8">
+        <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
+        <button
+          className="px-4 py-1 sm:px-7 sm:py-2
+          bg-red-800 hover:bg-primary-dull transition rounded-full
+        font-medium cursor-pointer"
+        >
+          Login
+        </button>
       </div>
-      <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer'/>
+      <MenuIcon className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" />
     </div>
   );
 };
